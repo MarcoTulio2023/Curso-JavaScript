54848ef58935ed4a2a53ed0b0f6cf94e4181cd58
MarcoTulio2023 <marcotuliofreitas110@gmail.com>
2024-11-02 11:29:59 -0300

    Primeiro commit

---
+++ Atividades/atividade01.js
@@ -0,0 +1,14 @@
+//Faça um algoritmo que receba 2 números e ao final mostre a soma subtração multiplicação e a divisão dos números lidos
+
+var n1 = 1
+var n2 = 0
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+alert("A soma dos dois números é: " + soma)
+alert("A subtração dos dois números é: " + subtracao)
+alert("A multiplicação dos dois números é: " + multiplicacao)
+alert("A divisão dos dois números é: " + divisao)
